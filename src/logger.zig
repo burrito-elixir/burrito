943cd57ba62c4ea0da0f77602d8127f48c7bda87
Digit Dog <woof@puppy.surf>
2022-11-06 12:56:56 -0800

    fix logger not printing messages properly in wrapped bins

--- src/logger.zig
+++ src/logger.zig
@@ -6,30 +6,30 @@ var allocator = arena.allocator();
 
 pub fn query(comptime message: []const u8, args: anytype) void {
     var stdout = std.io.getStdOut().writer();
-    var out_string = std.fmt.allocPrint(allocator, message, args);
-    stdout.print("[?] {any}", .{out_string}) catch {};
+    var out_string = std.fmt.allocPrint(allocator, message, args) catch { return; };
+    stdout.print("[?] {s}", .{out_string}) catch {};
 }
 
 pub fn info(comptime message: []const u8, args: anytype) void {
     var stdout = std.io.getStdOut().writer();
-    var out_string = std.fmt.allocPrint(allocator, message, args);
-    stdout.print("[i] {any}\n", .{out_string}) catch {};
+    var out_string = std.fmt.allocPrint(allocator, message, args) catch { return; };
+    stdout.print("[i] {s}\n", .{out_string}) catch {};
 }
 
 pub fn warn(comptime message: []const u8, args: anytype) void {
     var stderr = std.io.getStdErr().writer();
-    var out_string = std.fmt.allocPrint(allocator, message, args);
-    stderr.print("[w] {any}\n", .{out_string}) catch {};
+    var out_string = std.fmt.allocPrint(allocator, message, args) catch { return; };
+    stderr.print("[w] {s}\n", .{out_string}) catch {};
 }
 
 pub fn err(comptime message: []const u8, args: anytype) void {
     var stderr = std.io.getStdErr().writer();
-    var out_string = std.fmt.allocPrint(allocator, message, args);
-    stderr.print("[!] {any}\n", .{out_string}) catch {};
+    var out_string = std.fmt.allocPrint(allocator, message, args) catch { return; };
+    stderr.print("[!] {s}\n", .{out_string}) catch {};
 }
 
 pub fn crit(comptime message: []const u8, args: anytype) void {
     var stderr = std.io.getStdErr().writer();
-    var out_string = std.fmt.allocPrint(allocator, message, args);
-    stderr.print("[!!] {any}\n", .{out_string}) catch {};
+    var out_string = std.fmt.allocPrint(allocator, message, args) catch { return; };
+    stderr.print("[!!] {s}\n", .{out_string}) catch {};
 }
