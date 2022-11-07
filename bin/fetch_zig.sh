c5c0568e91343f1c5d19e3e9e9d526bd576d5ae7
Digit Dog <woof@puppy.surf>
2022-11-07 07:21:37 -0800

    update `bin/fetch_zig.sh` to auto-detect your OS and CPU arch

--- bin/fetch_zig.sh
+++ bin/fetch_zig.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
-ZIG_URL=$(curl -s https://ziglang.org/download/index.json | jq --raw-output ".master.\"x86_64-linux\".\"tarball\"")
-wget --show-progress -O zig.tar.xz $ZIG_URL
+ZIG_VERSION="0.10.0"
+
+####
+
+os_str=$(uname -s | tr '[:upper:]' '[:lower:]')
+arch_str=$(uname -m | tr '[:upper:]' '[:lower:]')
+
+if [ "$arch_str" = "arm64" ]; then
+    arch="aarch64"
+else
+    arch=$arch_str
+fi
+
+if [ "$os_str" = "darwin" ]; then
+    os="macos"
+else
+    os=$os_str
+fi
+
+zig_url=$(curl -s https://ziglang.org/download/index.json | jq --raw-output ".\"$ZIG_VERSION\".\"$arch-$os\".\"tarball\"")
+
+if [ "$zig_url" = "nul" ]; then
+    echo "Cannot auto-detect a Zig release for your combination of OS and CPU!"
+    exit 1
+fi
+
+echo "Downloading Zig ($ZIG_VERSION) for: $arch-$os..."
+
+wget --show-progress -O zig.tar.xz $zig_url
