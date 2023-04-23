110fd30d4a24a37121ee99a2a0b584908da30cf5
Digit Dog <woof@puppy.surf>
2023-04-23 14:00:07 -0700

    Delete old example app

--- example/test_plugin/plugin.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub fn burrito_plugin_entry(install_dir: []const u8, program_manifest_json: []const u8) void {
-    std.log.info("Test Plugin Init!", .{});
-    std.log.info("Test Plugin: {s}", .{install_dir});
-    std.log.info("Test Plugin: {s}", .{program_manifest_json});
-}
