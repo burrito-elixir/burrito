0b549d8779fe247dee958dd82e3a1b6217e931be
Nate Heydt <heydtn@gmail.com>
2023-09-04 07:47:17 -0700

    Refactor and clean up erlang launcher

--- wrapper/src/erlang_launcher.rs
+++ wrapper/src/erlang_launcher.rs
@@ -4,17 +4,25 @@
 */
 
 use anyhow::Result;
-use paris::error;
 use std::fs;
 
 #[cfg(unix)]
 use std::os::unix::process::CommandExt;
 
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{abort, exit, Command};
 
 use crate::archiver::PayloadMetadata;
 use crate::errors::WrapperError;
+
+macro_rules! build_path {
+    ($root:expr,$path:expr) => {
+        buf_to_string($root.join($path))
+    };
+    ($root:expr, $pattern:expr, $($value:expr),+) => {
+        buf_to_string($root.join(format!($pattern, $($value),+)))
+    };
+}
 
 pub fn launch_app(
     install_dir: &Path,
@@ -23,34 +31,29 @@ pub fn launch_app(
 ) -> Result<(), WrapperError> {
     let erl_bin_name = if cfg!(windows) { "erl.exe" } else { "erlexec" };
 
-    let release_cookie_path = Path::join(install_dir, "releases/COOKIE");
-    let release_lib_path = Path::join(install_dir, "lib/");
-    let install_vm_args_path = buf_to_string(Path::join(
-        install_dir,
-        format!("releases/{}/vm.args", release_meta.app_version),
-    ))?;
+    let release_cookie_path = build_path!(install_dir, "releases/COOKIE")?;
+    let release_lib_path = build_path!(install_dir, "lib/")?;
 
-    let config_sys_path = buf_to_string(Path::join(
-        install_dir,
-        format!("releases/{}/sys.config", release_meta.app_version),
-    ))?;
+    let install_vm_args_path =
+        build_path!(install_dir, "releases/{}/vm.args", release_meta.app_version)?;
 
-    let config_sys_path_no_ext = Path::join(
+    let config_sys_path = build_path!(
         install_dir,
-        format!("releases/{}/sys", release_meta.app_version),
-    );
-    let rel_vsn_dir = Path::join(
-        install_dir,
-        format!("releases/{}", release_meta.app_version),
-    );
-    let boot_path = buf_to_string(Path::join(&rel_vsn_dir, "start"))?;
+        "releases/{}/sys.config",
+        release_meta.app_version
+    )?;
+
+    let config_sys_path_no_ext =
+        build_path!(install_dir, "releases/{}/sys", release_meta.app_version)?;
+
+    let boot_path = build_path!(install_dir, "releases/{}/start", release_meta.app_version)?;
 
     let erts_version_name = format!("erts-{}", release_meta.erts_version);
-    let erts_bin_path = Path::join(install_dir, format!("{}/bin", erts_version_name));
-    let erl_bin_path = Path::join(&erts_bin_path, erl_bin_name);
+    let erts_bin_path = build_path!(install_dir, "{}/bin", erts_version_name)?;
+    let erl_bin_path = build_path!(install_dir, "{}/bin/{}", erts_version_name, erl_bin_name)?;
 
     let release_cookie_file =
-        fs::read_to_string(release_cookie_path).or(Err(WrapperError::LaunchCookieReadError))?;
+        fs::read_to_string(release_cookie_path).map_err(|_| WrapperError::LaunchCookieReadError)?;
 
     let mut executable = Command::new(erl_bin_path);
     executable
@@ -70,44 +73,47 @@ pub fn launch_app(
         .arg("-config")
         .arg(config_sys_path);
 
-    if cfg!(windows) {
-        executable
-            .env("RELEASE_ROOT", install_dir)
-            .env("RELEASE_SYS_CONFIG", config_sys_path_no_ext);
-    } else {
+    executable
+        .env("RELEASE_ROOT", install_dir)
+        .env("RELEASE_SYS_CONFIG", config_sys_path_no_ext);
+
+    if !cfg!(windows) {
         executable
             .env("ROOTDIR", install_dir)
             .env("BINDIR", erts_bin_path)
-            .env("RELEASE_ROOT", install_dir)
-            .env("RELEASE_SYS_CONFIG", config_sys_path_no_ext)
             .env("EMU", "beam")
             .env("PROGNAME", "erl");
     }
 
-    #[cfg(unix)]
-    {
-        let exec_error = executable.arg("-extra").args(sys_args).exec();
-        error!("Erlang Exec Error: {}", exec_error);
-    }
+    executable.arg("-extra").args(sys_args);
 
-    #[cfg(windows)]
-    {
-        match executable.arg("-extra").args(sys_args).spawn() {
-            Ok(mut child_process) => {
-                let _ = child_process.wait();
-                return Ok(());
-            },
-            Err(exec_error) => {
-                error!("Erlang Exec Error: {}", exec_error);
-            }
-        }
+    if cfg!(unix) {
+        handle_unix_exec(&mut executable)
+    } else if cfg!(windows) {
+        handle_windows_exec(&mut executable)
+    } else {
+        Err(WrapperError::LaunchUnsupportedPlatformError)
     }
+}
 
-    Err(WrapperError::LaunchError)
+fn handle_unix_exec(executable: &mut Command) -> Result<(), WrapperError> {
+    let exec_error = executable.exec();
+    Err(WrapperError::LaunchErlangError(exec_error))
+}
+
+fn handle_windows_exec(executable: &mut Command) -> Result<(), WrapperError> {
+    executable
+        .spawn()
+        .and_then(|mut child_process| child_process.wait())
+        .map(|exit_status| match exit_status.code() {
+            Some(code) => exit(code),
+            None => abort(),
+        })
+        .map_err(|exec_error| WrapperError::LaunchErlangError(exec_error))
 }
 
 fn buf_to_string(buf: PathBuf) -> Result<String, WrapperError> {
     buf.into_os_string()
         .into_string()
-        .or(Err(WrapperError::LaunchError))
+        .map_err(|_| WrapperError::LaunchEncodingError)
 }
