550e56a6aa64259abcf65c64691383b165254396
Nate Heydt <heydtn@gmail.com>
2023-09-04 11:15:34 -0700

    Refactor errors to improve return types for different parts of wrapper execution, refactor main into more of a pipeline.

--- wrapper/src/errors.rs
+++ wrapper/src/errors.rs
@@ -2,33 +2,59 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum WrapperError {
-    // Errors relating to the payload extraction
+    #[error("Error parsing metadata: {0}")]
+    ParseMetadataError(MetadataError),
+
+    #[error("Error computing the base install directory: {0}")]
+    ComputeInstallDirectoryError(ExtractError),
+
+    #[error("Error unpacking payload: {0}")]
+    DecompressPayloadError(ExtractError),
+
+    #[error("Failed to launch inner application: {0}")]
+    LaunchError(LauncherError),
+}
+
+// Errors relating to the payload extraction
+#[derive(Error, Debug)]
+pub enum ExtractError {
     #[error("Failed to decompress the payload, it may be damaged!")]
-    PayloadDecompressFailed,
+    PayloadDecompressError,
+    #[error("Failed to parse the decompressed payload, it may be damaged!")]
+    PayloadParseError,
     #[error("Could not create the destination directory for the file: {0}")]
-    ExtractMkdirFailed(String),
-    #[error("Could not write the file: {0}")]
-    ExtractFileWriteFailed(String),
+    MkdirError(std::io::Error),
+    #[error("Could not create file: {0}")]
+    FileCreateError(std::io::Error),
+    #[error("Could not write data to file: {0}")]
+    FileWriteError(std::io::Error),
     #[error("Failed to set the permissions/mode on file: {0}")]
-    ExtractChmodFailed(String),
+    ChmodError(std::io::Error),
     #[error("Invalid installation directory")]
-    ExtractInvalidInstallDir,
+    InvalidInstallDirError,
     #[error(
         "Could not compute a valid installation directory, your platform may not be supported!"
     )]
-    ExtractCannotComputeInstallDir,
+    CannotComputeInstallDirError,
+}
 
+// Errors relating to metadata validation
+#[derive(Error, Debug)]
+pub enum MetadataError {
     // Metadata related errors
     #[error("Metadata JSON is corrupted!")]
-    MetadataCorrupted,
+    CorruptedError,
+}
 
-    // Erlang launcher related errors
+// Erlang launcher related errors
+#[derive(Error, Debug)]
+pub enum LauncherError {
     #[error("Could not read the COOKIE file for the release!")]
-    LaunchCookieReadError,
+    CookieReadError,
     #[error("Erlang launcher error. Please file an issue with the Burrito project.")]
-    LaunchEncodingError,
+    EncodingError,
     #[error("Erlang exec error: {0}")]
-    LaunchErlangError(#[from] std::io::Error),
+    ErlangError(std::io::Error),
     #[error("Could not launch the application, your host platform is not supported!")]
-    LaunchUnsupportedPlatformError,
+    UnsupportedPlatformError,
 }
