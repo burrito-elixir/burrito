723533e94680b36eda7ac1c1e314c777a95d0297
Nate Heydt <heydtn@gmail.com>
2023-09-22 12:33:09 -0700

    Move Release specific logic into a shared location

--- wrapper/src/errors.rs
+++ wrapper/src/errors.rs
@@ -2,25 +2,22 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum WrapperError {
-    #[error("Error parsing metadata: {0}")]
-    ParseMetadataError(MetadataError),
+    #[error("Failed to load release information: {0}")]
+    LoadReleaseError(#[from] ReleaseError),
 
-    #[error("Error computing the base install directory: {0}")]
-    ComputeInstallDirectoryError(ExtractError),
-
-    #[error("Error unpacking payload: {0}")]
-    DecompressPayloadError(ExtractError),
+    #[error("Failed to unpack payload: {0}")]
+    DecompressPayloadError(#[from] ExtractError),
 
     #[error("Failed to launch inner application: {0}")]
-    LaunchError(LauncherError),
+    LaunchError(#[from] LauncherError),
 }
 
 // Errors relating to the payload extraction
 #[derive(Error, Debug)]
 pub enum ExtractError {
-    #[error("Failed to decompress the payload, it may be damaged!")]
+    #[error("Could not decompress the payload, it may be damaged!")]
     PayloadDecompressError,
-    #[error("Failed to parse the decompressed payload, it may be damaged!")]
+    #[error("Could not parse the decompressed payload, it may be damaged!")]
     PayloadParseError,
     #[error("Could not create the destination directory for the file: {0}")]
     MkdirError(std::io::Error),
@@ -28,33 +25,34 @@ pub enum ExtractError {
     FileCreateError(std::io::Error),
     #[error("Could not write data to file: {0}")]
     FileWriteError(std::io::Error),
-    #[error("Failed to set the permissions/mode on file: {0}")]
+    #[error("Could not set the permissions/mode on file: {0}")]
     ChmodError(std::io::Error),
     #[error("Invalid installation directory")]
     InvalidInstallDirError,
+}
+
+// Errors relating to parsing releases and release paths
+#[derive(Error, Debug)]
+pub enum ReleaseError {
     #[error(
         "Could not compute a valid installation directory, your platform may not be supported!"
     )]
-    CannotComputeInstallDirError,
-}
-
-// Errors relating to metadata validation
-#[derive(Error, Debug)]
-pub enum MetadataError {
-    // Metadata related errors
+    ComputeInstallDirError,
+    #[error("Could not read the COOKIE file for the release!")]
+    CookieReadError,
     #[error("Metadata JSON is corrupted!")]
-    CorruptedError,
+    CorruptedMetadataError,
 }
 
 // Erlang launcher related errors
 #[derive(Error, Debug)]
 pub enum LauncherError {
-    #[error("Could not read the COOKIE file for the release!")]
-    CookieReadError,
-    #[error("Erlang launcher error. Please file an issue with the Burrito project.")]
-    EncodingError,
     #[error("Erlang exec error: {0}")]
-    ErlangError(std::io::Error),
+    ErlangError(#[from] std::io::Error),
+    #[error("Invalid encoding in {0}")]
+    EncodingError(String),
     #[error("Could not launch the application, your host platform is not supported!")]
     UnsupportedPlatformError,
+    #[error("Could not load wrapper information: {0}")]
+    LoadReleaseError(#[from] ReleaseError),
 }
