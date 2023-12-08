231f32ae0cd2c2f1269868482b1bb0993f665b1c
Digit Dog <woof@puppy.surf>
2023-12-07 18:23:18 -0800

    Add hex package description

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Burrito.MixProject do
   def project do
     [
       app: :burrito,
+      description:
+        "Burrito is our answer to the problem of distributing Elixir applications across varied environments. Turn your Elixir application into a simple, self-contained, single-file executable for MacOS, Linux, and Windows.",
       version: String.trim(File.read!("VERSION")),
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
@@ -12,7 +14,7 @@ defmodule Burrito.MixProject do
         main: "README",
         extras: ["README.md"]
       ],
-      package: package(),
+      package: package()
     ]
   end
 
