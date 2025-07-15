07434be44e65ae87fd87ed04ae4f0ea4abde5a30
sabiwara <sabiwara@gmail.com>
2025-07-15 10:27:25 +0900

    Add links to source in docs

--- mix.exs
+++ mix.exs
@@ -1,18 +1,23 @@
 defmodule Burrito.MixProject do
   use Mix.Project
 
+  @version String.trim(File.read!("VERSION"))
+  @github_url "https://github.com/burrito-elixir/burrito"
+
   def project do
     [
       app: :burrito,
       description:
         "Burrito is our answer to the problem of distributing Elixir applications across varied environments. Turn your Elixir application into a simple, self-contained, single-file executable for MacOS, Linux, and Windows.",
-      version: String.trim(File.read!("VERSION")),
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
         main: "readme",
-        extras: ["README.md"]
+        extras: ["README.md"],
+        source_url: @github_url,
+        source_ref: "v#{@version}"
       ],
       package: package()
     ]
@@ -32,7 +37,7 @@ defmodule Burrito.MixProject do
       files:
         ~w(lib LICENSE VERSION mix.exs README.md .formatter.exs src bin _dummy_plugin.zig build.zig),
       links: %{
-        "Github" => "https://github.com/burrito-elixir/burrito",
+        "Github" => @github_url,
         "Sponsor" => "https://github.com/sponsors/doawoo"
       }
     ]
