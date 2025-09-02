e27f9f01b62e2ab33bb04b950b3e21fa1ff2eaa9
D-Berg <danielfjberg@gmail.com>
2025-09-02 18:22:48 +0200

    fix getting stdout/stderr on windows
    having a global stdout/stderr is not supported on Windows

--- src/logger.zig
+++ src/logger.zig
@@ -1,39 +1,43 @@
 const std = @import("std");
 
-var stdout_buf: [64]u8 = undefined;
-var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
-const stdout = &stdout_writer.interface;
-
-var stderr_buf: [64]u8 = undefined;
-var stderr_writer = std.fs.File.stderr().writer(&stderr_buf);
-const stderr = &stderr_writer.interface;
-
 pub fn query(comptime message: []const u8, args: anytype) void {
-    stdout.print("[?] " ++ message, args) catch {};
-    stdout.flush() catch {};
+    printToStdout("[?] " ++ message, args);
 }
 
 pub fn log_stderr(comptime message: []const u8, args: anytype) void {
-    stderr.print("[l] " ++ message ++ "\n", args) catch {};
-    stderr.flush() catch {};
+    printToStderr("[l] " ++ message ++ "\n", args);
 }
 
 pub fn info(comptime message: []const u8, args: anytype) void {
-    stdout.print("[i] " ++ message ++ "\n", args) catch {};
-    stdout.flush() catch {};
+    printToStdout("[i] " ++ message ++ "\n", args);
 }
 
 pub fn warn(comptime message: []const u8, args: anytype) void {
-    stderr.print("[w] " ++ message ++ "\n", args) catch {};
-    stderr.flush() catch {};
+    printToStderr("[w] " ++ message ++ "\n", args);
 }
 
 pub fn err(comptime message: []const u8, args: anytype) void {
-    stderr.print("[!] " ++ message ++ "\n", args) catch {};
-    stderr.flush() catch {};
+    printToStderr("[!] " ++ message ++ "\n", args);
 }
 
 pub fn crit(comptime message: []const u8, args: anytype) void {
-    stderr.print("[!!] " ++ message ++ "\n", args) catch {};
+    printToStderr("[!!] " ++ message ++ "\n", args);
+}
+
+fn printToStdout(comptime message: []const u8, args: anytype) void {
+    var stdout_buf: [64]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
+    const stdout = &stdout_writer.interface;
+
+    stdout.print(message, args) catch {};
+    stdout.flush() catch {};
+}
+
+fn printToStderr(comptime message: []const u8, args: anytype) void {
+    var stderr_buf: [64]u8 = undefined;
+    var stderr_writer = std.fs.File.stderr().writer(&stderr_buf);
+    const stderr = &stderr_writer.interface;
+
+    stderr.print(message, args) catch {};
     stderr.flush() catch {};
 }
